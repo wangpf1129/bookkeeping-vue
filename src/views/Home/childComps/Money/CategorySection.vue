255d5555ab1324fa2ed7be5987319d8b9fc402b3
Wangpf <1204981117@qq.com>
2021-02-18 11:10:22 +0800

    按照Vue文档使用TS组件，并选择使用  vue-property-decorator这个库来简化书写

--- src/views/Home/childComps/Money/CategorySection.vue
+++ src/views/Home/childComps/Money/CategorySection.vue
@@ -1,26 +1,40 @@
 <template>
   <div class="category-wrapper">
-    <span class="selected">支出</span>
-    <span>收入</span>
+    <span :class="type === '-' && 'selected'" @click="selectType('-')">支出</span>
+    <span :class="type === '+' && 'selected'" @click="selectType('+')">收入</span>
   </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'CategorySection'
-  };
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class CategorySection extends Vue {
+    type = '-';  // '-' 表示支出， '+' 表示收入
+
+    selectType(type: string) {
+      if (type !== '-' && type !== '+') {
+        throw new Error('Type is unknown');
+      }
+      this.type = type;
+    }
+
+  }
 </script>
 
 <style lang="scss" scoped>
-.category-wrapper{
-  font-size: 18px;
-  padding: 3px 0;
-  >span{
-    margin-left: 8px;
-    padding: 8px;
+  .category-wrapper {
+    font-size: 18px;
+    padding: 3px 0;
+
+    > span {
+      margin-left: 8px;
+      padding: 8px;
+    }
+
+    .selected {
+      border-bottom: 2px solid #000;
+    }
   }
-  .selected{
-    border-bottom: 2px solid #000;
-  }
-}
 </style>
