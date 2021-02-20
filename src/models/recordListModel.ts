a3f22e49784e4bfdd27693cb8b1adbe9845a6b63
Wangpf <1204981117@qq.com>
2021-02-20 21:20:49 +0800

    设置提示语

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,20 +1,37 @@
 import clone from '@/common/clone';
 
 const localStorageName = 'recordList';
-const recordListModel = {
-  data: [] as RecordItem[],
 
-  create(record: RecordItem) {
+type RecordListModel = {
+  data: RecordItem[];
+  fetch: () => RecordItem[];
+  save: () => void;
+  create: (record: RecordItem) => boolean;
+}
+const recordListModel: RecordListModel = {
+  data: [],
+
+  create(record) {
+    if (record.tagIds.length === 0) {
+      alert('请输入标签!');
+      return false;
+    }
+    if (record.amount <= 0) {
+      alert('请输入金额!');
+      return false;
+    }
     const newRecord: RecordItem = clone(record);
     this.data.push(newRecord);
-    this.save()
+    this.save();
+    return true;
   },
+  
   fetch() {
     this.data = JSON.parse(window.localStorage.getItem(localStorageName) || '[]') as RecordItem[];
     return this.data;
   },
   save() {
-    return window.localStorage.setItem(localStorageName, JSON.stringify(this.data));
+    window.localStorage.setItem(localStorageName, JSON.stringify(this.data));
   }
 };
 
