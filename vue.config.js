6c1b63c115a993d560cbc2225bcfe648648ee336
Wangpf <1204981117@qq.com>
2021-02-22 23:21:48 +0800

    部署该项目

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const path = require('path')
 const Plugin = require('svg-sprite-loader/plugin')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/bookkeeping-vue-website/'
+    : '/',
   lintOnSave: false,
   chainWebpack: config => {
     const dir = path.resolve(__dirname, 'src/assets/icons');
