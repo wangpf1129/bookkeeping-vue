6f1114b11d267a1a7eed7ae50afdd99da13cb9f3
Wangpf <1204981117@qq.com>
2021-02-22 21:24:09 +0800

    安装echarts，并自定义一个 Chart组件（没有使用vue-echarts插件）

---
+++ src/components/common/Chart/Chart.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="chart_wrapper" ref="chartDiv">chart</div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component, Prop} from 'vue-property-decorator';
+  import * as echarts from 'echarts';
+
+  @Component
+  export default class Chart extends Vue {
+    @Prop() options?: any;
+
+    mounted() {
+      if (this.options === undefined) {
+        return console.log('options not found');
+      }
+      const chart = echarts.init(this.$refs.chartDiv as HTMLDivElement);
+      chart.setOption(this.options);
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .chart_wrapper {
+    height: 400px;
+  }
+</style>
