9a84c4076ae7cb3c8e966b47bd2dbe38c54356f9
Wangpf <1204981117@qq.com>
2021-02-22 22:10:30 +0800

    饼状图渲染

--- src/views/Statistics/childComps/ExpensesChart.vue
+++ src/views/Statistics/childComps/ExpensesChart.vue
@@ -5,6 +5,7 @@
       <span slot="pay_slot" class="pay">￥{{expensesALL}}</span>
       <span slot="count_slot" class="count">共{{$store.getters.expenses.length}}条支出项目</span>
     </ShowMoney>
+    <Chart :options="Option"/>
   </div>
 </template>
 
@@ -12,18 +13,46 @@
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
   import ShowMoney from '@/components/common/ShowMoney/ShowMoney.vue';
+  import {getData} from '@/common/getDate';
+  import Chart from '@/components/common/Chart/Chart.vue';
 
   @Component({
-    components: {ShowMoney}
+    components: {Chart, ShowMoney}
   })
   export default class ExpensesChart extends Vue {
-    // 获取总支出
     get expensesALL() {
       return (this.$store.getters.expenses as RecordItem[])
               .map(item => item.amount).reduce((preMoney, amount) => {
                 return preMoney += amount;
               }, 0);
     }
+
+    get Option() {
+      const {expensesALL} = this;
+      const {expenses, getItemName} = this.$store.getters;
+      return {
+        tooltip: {
+          trigger: 'item',
+          formatter: '{b} : {d}%'
+        },
+        series: [
+          {
+            type: 'pie',
+            radius: ['25%', '50%'],
+            center: ['50%', '60%'],
+            data: getData(expenses, expensesALL, getItemName),
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
+        ]
+      };
+    }
+
   }
 </script>
 
--- src/views/Statistics/childComps/IncomeChart.vue
+++ src/views/Statistics/childComps/IncomeChart.vue
@@ -5,6 +5,7 @@
       <span slot="pay_slot" class="pay">￥{{incomeAll}}</span>
       <span slot="count_slot" class="count">共{{$store.getters.income.length}}条收入项目</span>
     </ShowMoney>
+    <Chart :options="Option"/>
   </div>
 </template>
 
@@ -12,18 +13,46 @@
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
   import ShowMoney from '@/components/common/ShowMoney/ShowMoney.vue';
+  import {getData} from '@/common/getDate';
+  import Chart from '@/components/common/Chart/Chart.vue';
 
   @Component({
-    components: {ShowMoney}
+    components: {Chart, ShowMoney}
   })
   export default class IncomeChart extends Vue {
-    // 获取总收入
     get incomeAll() {
       return (this.$store.getters.income as RecordItem[])
               .map(item => item.amount).reduce((preMoney, amount) => {
                 return preMoney += amount;
               }, 0);
     }
+
+    get Option() {
+      const {incomeAll} = this;
+      const {income, getItemName} = this.$store.getters;
+      return {
+        tooltip: {
+          trigger: 'item',
+          formatter: '{b} : {d}%'
+        },
+        series: [
+          {
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: getData(income, incomeAll, getItemName),
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
+        ]
+      };
+    }
+
   }
 </script>
 
