dc0cef43dec5beffa534ef7450d40b6cf21e3bd1
wangpf <wangpf2051@gmail.com>
2022-12-23 10:52:28 +0800

    fix build file

--- vue.config.js
+++ vue.config.js
@@ -2,19 +2,20 @@ const path = require('path')
 const Plugin = require('svg-sprite-loader/plugin')
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/bookkeeping-vue-website/'
-    : '/',
   lintOnSave: false,
-  chainWebpack: config => {
-    const dir = path.resolve(__dirname, 'src/assets/icons');
+  chainWebpack: (config) => {
+    const dir = path.resolve(__dirname, 'src/assets/icons')
 
     config.module
       .rule('svg-sprite')
       .test(/\.svg$/)
-      .include.add(dir).end() // 包含icons目录
-      .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end();
-    config.plugin('svg-sprite').use(Plugin, [{plainSprite: true}]);
-    config.module.rule('svg').exclude.add(dir);  // 其他 svg loader 排除 icons目录
-  }
-};
+      .include.add(dir)
+      .end() // 包含icons目录
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({ extract: false })
+      .end()
+    config.plugin('svg-sprite').use(Plugin, [{ plainSprite: true }])
+    config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons目录
+  },
+}
